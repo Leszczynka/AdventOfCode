fdf79ebfbd7349d083c96ee04e90aab1abc7bc3f
leszczynka <karolina.leszczynska05@gmail.com>
2022-12-02 12:43:43 +0100

    Day 2 part 2 - solution

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Here are my python solutions to the challenges in [Advent Of Code](www.adventofc
 
 ### 🗓 [2022](https://adventofcode.com/2022)
 
-| Days  | Puzzles                                                 | Solutions                               | Parts |
-| ----- | ------------------------------------------------------- |-----------------------------------------|-----|
-| Day 1 | [Calorie Counting](https://adventofcode.com/2022/day/1) | [`day1.py`](/day1/day1.py) | ⭐⭐  |
+| Days  | Puzzles                                                    | Solutions                  | Parts |
+| ----- |------------------------------------------------------------|----------------------------|-------|
+| Day 1 | [Calorie Counting](https://adventofcode.com/2022/day/1)    | [`day1.py`](/day1/day1.py) | ⭐⭐    |
+| Day 2 | [Rock Paper Scissors](https://adventofcode.com/2022/day/2) | [`day2.py`](/day2/day2.py) | ⭐⭐    |
--- day2/day2.py
+++ day2/day2.py
@@ -6,19 +6,26 @@ def process_data(filename):
 
 def count_total_score():
     strategy_data = process_data('input.txt')
-    my_score = 0
+    my_score_part1 = 0
+    my_score_part2 = 0
     choice_points = {'X': 1, 'Y': 2, 'Z': 3}
     results = {
         ('A', 'X'): 3, ('A', 'Y'): 6, ('A', 'Z'): 0,
         ('B', 'X'): 0, ('B', 'Y'): 3, ('B', 'Z'): 6,
         ('C', 'X'): 6, ('C', 'Y'): 0, ('C', 'Z'): 3,
     }
+    my_choices = {(k[0], v): k[1] for k, v in results.items()}
+    print(my_choices)
+    strategy = {'X': 0, 'Y': 3, 'Z': 6}
     for game_round in strategy_data:
         opponent = game_round[0]
         me = game_round[1]
-        my_score += choice_points[me] + results[opponent, me]
-    return my_score
+        my_score_part1 += choice_points[me] + results[opponent, me]
+        my_score_part2 += choice_points[my_choices[(opponent, strategy[me])]] + strategy[me]
+    return my_score_part1, my_score_part2
 
 
 if __name__ == '__main__':
-    print(f'Part one: total score={count_total_score()}.')
+    part1, part2 = count_total_score()
+    print(f'Part one: total score={part1}.')
+    print(f'Part two: total score={part2}.')
