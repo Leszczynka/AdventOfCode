b34595ffd7b07205c0d098010c7bb26f421719e1
leszczynka <karolina.leszczynska05@gmail.com>
2022-12-03 11:38:40 +0100

    Day 3 part 2 - solution & code refactoring

--- day3/day3.py
+++ day3/day3.py
@@ -8,12 +8,9 @@ def process_data(filename):
         return rucksacks_items
 
 
-def calculate_sum_of_priorities():
+def find_items_types():
     rucksacks_items = process_data('input.txt')
-
-    lower_priority = {k: v for (v, k) in enumerate(string.ascii_lowercase, start=1)}
-    upper_priority = {k: v for (v, k) in enumerate(string.ascii_uppercase, start=27)}
-    priority_sum = 0
+    shared_items = []
 
     for items in rucksacks_items:
         n = len(items)
@@ -21,14 +18,45 @@ def calculate_sum_of_priorities():
         first_half = set(items[0:half])
         second_half = set(items[half:])
         shared_item = ''.join(first_half & second_half)
+        shared_items.append(shared_item)
 
-        if shared_item.islower():
-            priority_sum += lower_priority[shared_item]
+    return shared_items
+
+
+def find_items_types_corresponded_to_the_groups():
+    rucksacks_items = process_data('input.txt')
+    groups = [rucksacks_items[x:x+3] for x in range(0, len(rucksacks_items), 3)]
+    badge_items = []
+
+    for group in groups:
+        elf1 = set(group[0])
+        elf2 = set(group[1])
+        elf3 = set(group[2])
+        group_badge = ''.join(elf1 & elf2 & elf3)
+        badge_items.append(group_badge)
+
+    return badge_items
+
+
+def calculate_sum_of_items_priorities(items):
+    lower_priority = {k: v for (v, k) in enumerate(string.ascii_lowercase, start=1)}
+    upper_priority = {k: v for (v, k) in enumerate(string.ascii_uppercase, start=27)}
+    priority_sum = 0
+
+    for item in items:
+        if item.islower():
+            priority_sum += lower_priority[item]
         else:
-            priority_sum += upper_priority[shared_item]
+            priority_sum += upper_priority[item]
 
     return priority_sum
 
 
 if __name__ == '__main__':
-    print(f'Part one: Sum of the priorities of items shared in each rucksack is: {calculate_sum_of_priorities()}.')
+    part1_items = find_items_types()
+    part1_sum = calculate_sum_of_items_priorities(part1_items)
+    print(f'Part one: Sum of the priorities of item types is: {part1_sum}.')
+
+    part2_items = find_items_types_corresponded_to_the_groups()
+    part2_sum = calculate_sum_of_items_priorities(part2_items)
+    print(f'Part two: Sum of the priorities of groups item types is: {part2_sum}.')
