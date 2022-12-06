8b9cbe2bd12ea016084ad683c96aa85ce7816fbd
leszczynka <karolina.leszczynska05@gmail.com>
2022-12-06 09:48:40 +0100

    Day 6 part 2 - solution.

--- day6/day6.py
+++ day6/day6.py
@@ -8,9 +8,15 @@ def calculate_number_of_processed_char():
     characters = process_data('input.txt')
     for i in range(len(characters) - 4):
         if len(set(characters[i:i+4])) == 4:
-            return i + 4
+            start_of_packet_marker = i + 4
+    for i in range(len(characters) - 14):
+        if len(set(characters[i:i+14])) == 14:
+            start_of_message_marker = i + 14
+
+    return start_of_packet_marker, start_of_message_marker
 
 
 if __name__ == '__main__':
-    part_one = calculate_number_of_processed_char()
-    print(f'First marker was detected after processing {part_one} characters.')
+    part_one, part_two = calculate_number_of_processed_char()
+    print(f'First start-of-packet marker was detected after processing {part_one} characters.')
+    print(f'First start-of-message-marker was detected after processing {part_two} characters.')
